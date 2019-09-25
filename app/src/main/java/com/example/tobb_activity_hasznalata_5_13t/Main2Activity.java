190e032490acfcc1719dd5401ba0c8ecc1f659e1
lambortomi <lambortomi@gmail.com>
2019-09-25 13:38:19 +0200

    Második activity hozzáadása

---
+++ app/src/main/java/com/example/tobb_activity_hasznalata_5_13t/Main2Activity.java
@@ -0,0 +1,14 @@
+package com.example.tobb_activity_hasznalata_5_13t;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Main2Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+    }
+}
