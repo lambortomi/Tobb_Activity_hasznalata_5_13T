c2a80045328d62147b38ab48e4eb5f312a54893a
lambortomi <lambortomi@gmail.com>
2019-09-25 14:36:30 +0200

    Második Activity kész,project kész

--- app/src/main/java/com/example/tobb_activity_hasznalata_5_13t/Main2Activity.java
+++ app/src/main/java/com/example/tobb_activity_hasznalata_5_13t/Main2Activity.java
@@ -2,13 +2,30 @@ package com.example.tobb_activity_hasznalata_5_13t;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Main2Activity extends AppCompatActivity {
+
+    Button buttonVissza;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+
+        buttonVissza = findViewById(R.id.buttonVissza);
+        buttonVissza.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent vissza = new Intent(Main2Activity.this, MainActivity.class);
+
+                startActivity(vissza);
+                finish();
+            }
+
+        });
     }
 }
--- app/src/main/java/com/example/tobb_activity_hasznalata_5_13t/MainActivity.java
+++ app/src/main/java/com/example/tobb_activity_hasznalata_5_13t/MainActivity.java
@@ -2,13 +2,32 @@ package com.example.tobb_activity_hasznalata_5_13t;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button buttonTovabb;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        buttonTovabb = findViewById(R.id.buttonTovabb);
+        buttonTovabb.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent tovabb = new Intent(MainActivity.this, Main2Activity.class);
+
+                startActivity(tovabb);
+                //finish();
+            }
+
+        });
     }
+
+
 }
